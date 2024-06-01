9803290f9a0142da46096990e8aec3a9b2bf57df
Nesto <ernestohlacalle@gmail.com>
2024-06-01 12:45:58 +0200

    assign values to the struct

--- pointers/main.c
+++ pointers/main.c
@@ -3,19 +3,57 @@
 
 typedef struct {
   int degree;
-  float *coef;
+  int *coef;
 } tpolynomial;
 
-int readPolynomial();
+
+tpolynomial* create();
+int readPolynomial(tpolynomial *polynomial);
+void inspectPolynomial(tpolynomial *polynomial);
+void cleanKeyboardInput();
+
 
 int main() {
-  readDegree();
-  readPolynomial();
+  tpolynomial *firstPolynomial, *secondPolynomial;
+
+  firstPolynomial = create();
+  secondPolynomial = create();
+
+  inspectPolynomial(firstPolynomial);
+  inspectPolynomial(secondPolynomial);
+
+  free(firstPolynomial);
+  free(secondPolynomial);
+
+  return 0;
 }
 
-int readPolynomial() {
-  int result = 1, input, *pintArr, *temp, size = 0;
-  pintArr = malloc(1 * sizeof(int));
+tpolynomial* create() {
+  tpolynomial *newPolynomial;
+  newPolynomial = malloc(sizeof(tpolynomial));
+
+  readPolynomial(newPolynomial);
+
+  return newPolynomial;
+}
+
+void inspectPolynomial(tpolynomial *polynomial) {
+  if(!polynomial->degree) {
+    printf("Polynomial is empty \n");  
+  }
+
+
+  printf("Polynomial degree is: %d \n", polynomial->degree);
+  printf("Polynomial coefficients are: \n");
+
+  for(int i = 0; i < polynomial->degree; i++) {
+    printf("%d^%d: \n", *(polynomial->coef + i), polynomial->degree - i);
+  }
+}
+
+int readPolynomial(tpolynomial *polynomial) {
+  int result = 1, input, *temp, size = 0;
+  polynomial->coef = (int *)malloc(1 * sizeof(int));
 
   printf("Write any number, type any word to exit:\n");
 
@@ -23,19 +61,28 @@ int readPolynomial() {
     result = scanf("%d", &input);
 
     if(result) {
-      int *temp = realloc(pintArr, (size + 1) * sizeof(int));
+      int *temp = realloc(polynomial->coef, (size + 1) * sizeof(int));
 
       if(temp == NULL) {
         printf("Error reallocating memory!\n");
-        free(pintArr);
+        free(polynomial->coef);
         return 1;
       }
 
-      pintArr = temp;
-      *(pintArr + size) = input;
+      polynomial->coef = temp;
+      *(polynomial->coef + size) = input;
       size++;
     }
   }
+
+  cleanKeyboardInput();
+
+  polynomial->degree = size;
+}
+
+void cleanKeyboardInput() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
 
