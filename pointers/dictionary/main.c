456a73855b8e621485cbcdb3f643d69d71d76f8f
Nesto <ernestohlacalle@gmail.com>
2024-06-10 19:29:11 +0200

    sort and compare

--- pointers/dictionary/main.c
+++ pointers/dictionary/main.c
@@ -2,23 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void sort(char **list);
+int gt(char *word, char *otherWord);
+
 #define MAX_SIZE 255
 int main() {
-    char *row, buffer[MAX_SIZE];
-    char **pointer = malloc(sizeof(char *));
-    int wordLength, size = 0;
+    char *list[] = { "hola", "mundo", "esto", "es", "c", NULL };
 
-    while (size <= 2) {
-        printf("Write a word:");
-        scanf("%s", buffer);
-        wordLength = strlen(buffer);
+    sort(list);
 
-        row = malloc(wordLength * sizeof(char*));
-        strcpy(row, buffer);
-        *(pointer + size) = row;
+    for(int i = 0; i < 5; i++) {
+        printf("%s \n", list[i]);
+    }
+}
 
-        size++;
-        pointer = (char **) realloc(pointer, (size + 1) * sizeof(char *));
+void sort(char **list) {
+    int length = 5, swapped = 0;
+    char *temp;
+
+    do {
+        swapped = 0;
+
+        for(int i = 0; i + 1 < length; i++) { 
+            if(gt(list[i], list[i + 1])) {
+                temp = *(list + i);
+                *(list + i) = *(list + i + 1);
+                *(list + i + 1) = temp;
+                
+                swapped = 1;
+            }
+        }
+
+        length--;
+    }
+    while (swapped);
+}
+
+int gt(char *word, char *otherWord) {
+    if (word[strlen(word) - 1] < otherWord[strlen(otherWord) - 1]) {
+        return 1;
     }
 
     return 0;
