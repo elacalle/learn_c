8c3be75aedb5e6ec3086029b3df9f5c16a09b08d
Nesto <ernestohlacalle@gmail.com>
2024-06-10 21:36:48 +0200

    sort dynamic dictionary

--- pointers/dictionary/main.c
+++ pointers/dictionary/main.c
@@ -3,46 +3,84 @@
 #include <string.h>
 
 
-void sort(char **list);
+void sort(char ***list, int *size);
 int gt(char *word, char *otherWord);
+int input(char ***list, int *size);
+void cleanKeyboardInput();
 
 #define MAX_SIZE 255
 int main() {
-    char *list[] = { "hola", "mundo", "esto", "es", "c", NULL };
+    int size = 0; 
+    char **list = malloc(sizeof(char *));
 
-    sort(list);
+    input(&list,  &size);
+    sort(&list, &size);
 
-    for(int i = 0; i < 5; i++) {
-        printf("%s \n", list[i]);
+    for(int i = 0; i < size; i++) {
+        printf("%s\n", list[i]);
     }
 }
 
-void sort(char **list) {
-    int length = 5, swapped = 0;
+int input(char ***list, int *size) {
+    int result = 1;
+    char word[MAX_SIZE], *newWord;
+    char **tempDir;
+    
+    printf("Write a word: \n");
+
+    while (1)
+    {
+        scanf("%s", word);
+
+        if (strcmp(word, "exit") == 0) {
+            break;
+        }
+
+        newWord = malloc(strlen(word) * sizeof(char));
+        strcpy(newWord, word);
+        (*list)[*size] = newWord;
+
+        *size = *size + 1;
+        tempDir = realloc(*list, (*size + 1) * sizeof(char *));
+
+        *list = tempDir; 
+        
+        cleanKeyboardInput();
+    }
+}
+
+void sort(char ***list, int *size) {
+    int length = *size;
+    int swapped;
     char *temp;
 
     do {
         swapped = 0;
 
-        for(int i = 0; i + 1 < length; i++) { 
-            if(gt(list[i], list[i + 1])) {
-                temp = *(list + i);
-                *(list + i) = *(list + i + 1);
-                *(list + i + 1) = temp;
-                
+        for (int i = 0; i + 1 < length; i++) {
+
+            if (gt((*list)[i], (*list)[i + 1])) {
+                temp = (*list)[i];
+                (*list)[i] = (*list)[i + 1];
+                (*list)[i + 1] = temp;
+
                 swapped = 1;
             }
         }
 
         length--;
-    }
-    while (swapped);
+    } while (swapped);
 }
 
 int gt(char *word, char *otherWord) {
-    if (word[strlen(word) - 1] < otherWord[strlen(otherWord) - 1]) {
+    if (word[strlen(word) - 1] > otherWord[strlen(otherWord) - 1]) {
         return 1;
     }
 
     return 0;
+}
+
+void cleanKeyboardInput() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
